f0434074695e88ae8ed3a65c459eb38f6c5ffcf6
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2025-03-03 18:05:31 +0000

    lib imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
 mod constants;
+mod error;
 mod protocol;
 pub mod request;
 pub mod response;
-mod result;
 mod server;
 mod status;
 mod transport;
 mod version;
 
+pub use error::*;
 pub use request::HttpRequest;
 pub use response::HttpResponse;
-pub use result::Result;
 pub use server::HttpServer;
 pub use status::HttpStatusCode;
 pub use version::HttpVersion;
+
+pub use transport::TcpTransport;
+
+pub type Result<T> = std::result::Result<T, Error>;
