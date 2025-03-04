3bc2837aea22977ca71512603cd303793d1398c2
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2025-03-04 00:04:33 +0000

    correct library exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,18 @@
 mod constants;
 mod error;
-mod protocol;
+pub mod protocol;
 pub mod request;
 pub mod response;
 mod server;
 mod status;
-mod transport;
+pub mod transport;
 mod version;
 
 pub use error::*;
 pub use request::HttpRequest;
 pub use response::HttpResponse;
-pub use server::HttpServer;
+pub use server::{HttpServer, YarsServer};
 pub use status::HttpStatusCode;
 pub use version::HttpVersion;
-
-pub use transport::TcpTransport;
 
 pub type Result<T> = std::result::Result<T, Error>;
